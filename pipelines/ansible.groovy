f5646b59ca068d16d432824d7532b320caf7e63c
N Ch <dwarakesh.n@thomsonreuters.com>
2020-05-09 18:53:59 +0530

    2. Again i have done some changes in the ansible.groovy

---
+++ pipelines/ansible.groovy
@@ -0,0 +1,26 @@
+def buildAgent = 'CentosNode'
+def playbook = "EmpDeployPlaybook.yml"
+
+timestamps
+{
+    node(buildAgent)
+    {  
+        stage('Select Env And Invoke Playbook')
+        {
+            {  dir('ansible')
+                {
+                    try{
+                        // ansiblePlaybook(
+                        //   playbook: "$playbook",inventory: "inventory"
+                        // )
+                      } 
+                      
+                    catch (e)
+                    {
+                        echo e.getMessage()
+                        currentBuild.result='FAILURE'
+                    }   
+            }
+        }
+    }
+}
