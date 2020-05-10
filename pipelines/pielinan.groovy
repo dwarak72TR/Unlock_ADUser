c1cfab29dcf8b0075b3394cdc3559342b76fca02
N Ch <dwarakesh.n@thomsonreuters.com>
2020-05-10 13:07:38 +0530

    Added two pipelines jobs

---
+++ pipelines/pielinan.groovy
@@ -0,0 +1,26 @@
+def buildAgent = 'CentosNode'
+def playbook = "ansiename.yaml"
+
+timestamps
+{
+    node(buildAgent)
+    {  
+        stage('Select Env And Invoke Playbook')
+        {
+          
+                    try{
+                        ansiblePlaybook(
+                         playbook: "$playbook"
+                        )
+                       // sh 'echo "for next time we will run the playbook not now"'
+                      } 
+                      
+                    catch (e)
+                    {
+                        echo e.getMessage()
+                        currentBuild.result='FAILURE'
+                    }   
+           
+        }
+    }
+}
